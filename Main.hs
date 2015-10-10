cc5d56b8841267682a9bec8424d34ca6007b7dde
Gabe Berke-Williams <gabebw@gabebw.com>
2015-10-10 15:13:09 -0700

    Aeson's inferred JSON parsing is so cool
    We use record syntax to give names to the `RhymebrainResult` JSON object fields
    that we want to pull out: `score` and `word`.
    
    With just the names of the fields, Aeson + GHC's generics can auto-derive a JSON
    parser so that `decode sample` Just Works.

--- Main.hs
+++ Main.hs
@@ -1,8 +1,19 @@
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE DeriveGeneric, DeriveAnyClass #-}
 
 import Control.Lens
 import Network.Wreq
 import Data.Text as T
+import Data.Aeson
+import GHC.Generics
+
+data RhymebrainResult = RhymebrainResult { score :: Int, word :: T.Text  }
+    deriving (Generic, FromJSON, Show)
+
+sample = "[ {\"word\":\"part\",\"freq\":27,\"score\":300,\"flags\":\"bc\",\"syllables\":\"1\"} ]"
+
+decodeSample :: Maybe [RhymebrainResult]
+decodeSample = decode sample
 
 rhymebrainOptions :: T.Text -> Options
 rhymebrainOptions word = defaults &
@@ -17,4 +28,5 @@ rhymebrainHost = "http://rhymebrain.com/talk"
 main = do
     let word = "heart"
     r <- getWith (rhymebrainOptions word) rhymebrainHost
-    print $ r ^. responseBody
+    -- print $ r ^. responseBody
+    print $ decodeSample
