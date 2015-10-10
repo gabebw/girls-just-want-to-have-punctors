77f35aae80a5468a21b06d856ffa7fcfa36d5e30
Gabe Berke-Williams <gabebw@gabebw.com>
2015-10-10 16:08:09 -0700

    Print the highest-scoring results from Rhymebrain

--- Main.hs
+++ Main.hs
@@ -7,6 +7,7 @@ import Data.Text as T (Text)
 import Data.Aeson
 import GHC.Generics
 import Data.Function (on)
+import Data.List (maximumBy)
 
 data RhymebrainResult = RhymebrainResult { score :: Int, word :: T.Text  }
     deriving (Generic, FromJSON, Show, Eq)
@@ -24,8 +25,15 @@ rhymebrainOptions word = defaults &
 rhymebrainHost :: String
 rhymebrainHost = "http://rhymebrain.com/talk"
 
+rhymebrainResults :: T.Text -> IO (Response [RhymebrainResult])
+rhymebrainResults word = asJSON =<< getWith (rhymebrainOptions word) rhymebrainHost
+
+resultsWithScore :: Int -> [RhymebrainResult] -> [RhymebrainResult]
+resultsWithScore s = filter (\result -> score result == s)
+
 main = do
     let word = "heart"
-    r <- getWith (rhymebrainOptions word) rhymebrainHost
-    -- print $ r ^. responseBody
-    print $ decodeSample
+    r <- rhymebrainResults word
+    let results = r ^. responseBody
+    let highestScoring = resultsWithScore (score $ maximum results) results
+    print $ highestScoring
