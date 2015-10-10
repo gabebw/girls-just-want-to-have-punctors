086dfcbbf7e55cc7a399bb05939d6a1ac8c5c842
Gabe Berke-Williams <gabebw@gabebw.com>
2015-10-10 15:01:59 -0700

    Get raw JSON from Rhymebrain

--- Main.hs
+++ Main.hs
@@ -2,7 +2,19 @@
 
 import Control.Lens
 import Network.Wreq
+import Data.Text as T
+
+rhymebrainOptions :: T.Text -> Options
+rhymebrainOptions word = defaults &
+    param "function" .~ ["getRhymes"] &
+    param "maxResults" .~ ["0"] &
+    param "lang" .~ ["en"] &
+    param "word" .~ [word]
+
+rhymebrainHost :: String
+rhymebrainHost = "http://rhymebrain.com/talk"
 
 main = do
-    r <- get "http://httpbin.org/get"
+    let word = "heart"
+    r <- getWith (rhymebrainOptions word) rhymebrainHost
     print $ r ^. responseBody
