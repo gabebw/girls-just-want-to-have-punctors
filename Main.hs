73fe82f0053f1577126c6f8d668d30ec25c66750
Gabe Berke-Williams <gabebw@gabebw.com>
2015-10-10 14:50:53 -0700

    Successfully make an HTTP request

--- Main.hs
+++ Main.hs
@@ -1,1 +1,8 @@
-main = putStrLn "It works!"
+{-# LANGUAGE OverloadedStrings #-}
+
+import Control.Lens
+import Network.Wreq
+
+main = do
+    r <- get "http://httpbin.org/get"
+    print $ r ^. responseBody
