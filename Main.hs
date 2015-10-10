213c2d446325c2207986d66568691d12c127000f
Gabe Berke-Williams <gabebw@gabebw.com>
2015-10-10 16:07:07 -0700

    Remove sample code

--- Main.hs
+++ Main.hs
@@ -10,10 +10,6 @@ import GHC.Generics
 data RhymebrainResult = RhymebrainResult { score :: Int, word :: T.Text  }
     deriving (Generic, FromJSON, Show)
 
-sample = "[ {\"word\":\"part\",\"freq\":27,\"score\":300,\"flags\":\"bc\",\"syllables\":\"1\"} ]"
-
-decodeSample :: Maybe [RhymebrainResult]
-decodeSample = decode sample
 
 rhymebrainOptions :: T.Text -> Options
 rhymebrainOptions word = defaults &
